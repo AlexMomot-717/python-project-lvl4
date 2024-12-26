69a1614c0c155e7daaa41a0ecc6a34e87ff38f6d
alexandra <momotav717@gmail.ru>
2024-12-26 19:02:27 +0300

    refactor(final_step_new): add labels to forms fields

--- task_manager/tasks/forms.py
+++ task_manager/tasks/forms.py
@@ -1,8 +1,23 @@
 from django import forms
+from django.utils.translation import gettext as _
+from task_manager.labels.models import Label
+from task_manager.statuses.models import Status
 from task_manager.tasks.models import Task
+from task_manager.users.models import ServiceUser
 
 
 class TaskForm(forms.ModelForm[Task]):
+    name = forms.CharField(label=_("Name"))
+    description = forms.CharField(
+        label=_("Description"), widget=forms.Textarea(), required=False
+    )
+    status = forms.ModelChoiceField(queryset=Status.objects.all(), label=_("Status"))
+    executor = forms.ModelChoiceField(
+        queryset=ServiceUser.objects.all(), label=_("Executor")
+    )
+    labels = forms.ModelMultipleChoiceField(
+        queryset=Label.objects.all(), label=_("Labels"), required=False
+    )
 
     class Meta:
         model = Task
--- task_manager/users/forms.py
+++ task_manager/users/forms.py
@@ -1,12 +1,19 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
+from django.utils.translation import gettext as _
 from task_manager.users.models import ServiceUser
 
 
 class ServiceUserForm(UserCreationForm[ServiceUser]):
-    first_name = forms.CharField()
-    last_name = forms.CharField()
+    first_name = forms.CharField(label=_("First name"))
+    last_name = forms.CharField(label=_("Last name"))
 
     class Meta:
         model = ServiceUser
-        fields = ("first_name", "last_name", "username", "password1", "password2")
+        fields = (
+            "first_name",
+            "last_name",
+            "username",
+            "password1",
+            "password2",
+        )
